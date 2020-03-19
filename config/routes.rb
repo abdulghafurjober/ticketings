164e421eec3c3ebd06ea3e4076a82442912e3ca7
abdulghafurjober <abdulghafurjober@hotmail.com>
2020-03-19 20:35:57 +0800

    authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
-  get 'main_page/index'
-  root 'main_page#index'
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  get 'dashboard/index'
+  root 'dashboard#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
