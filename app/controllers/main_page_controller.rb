65dddf304fa39167e629d554a3f308af41093bea
abdulghafurjober <abdulghafurjober@hotmail.com>
2020-03-19 15:32:36 +0800

    authentication

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -1,4 +1,5 @@
 class MainPageController < ApplicationController
+  before_action :authenticate_user!
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  devise_for :users
   get 'main_page/index'
   root 'main_page#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
