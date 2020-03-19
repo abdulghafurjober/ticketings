6ecb4b625d31c7497d8444411acbbf80f3b89f47
abdulghafurjober <abdulghafurjober@hotmail.com>
2020-03-19 18:56:21 +0800

    dummy page fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  get 'main_page/index'
-  root 'main_page#index'
+  get 'dashboard/index'
+  root 'dashboard#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class MainPageControllerTest < ActionDispatch::IntegrationTest
+class DashboardControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get main_page_index_url
+    get dashboard_index_url
     assert_response :success
   end
 
